dc2244762486467a612638b9b9a18886def55f49
georgealexandruiancu <iancugeorgealexandru@gmail.com>
2020-01-14 21:18:35 +0000

    ye

--- mq2.py
+++ mq2.py
@@ -22,8 +22,15 @@ SPICLK = 18
 SPIMISO = 23
 SPIMOSI = 24
 SPICS = 25
+
 mq7_dpin = 26
 mq7_apin = 0
+
+mq2_dpin = 26
+mq2_apin = 0
+
+smokesensor_dpin = 26
+smokesensor_apin = 0
 
 os.system("sudo motion")
 
@@ -44,7 +51,6 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         if ((adcnum > 7) or (adcnum < 0)):
                 return -1
         GPIO.output(cspin, True)	
-
         GPIO.output(clockpin, False)  # start clock low
         GPIO.output(cspin, False)     # bring CS low
 
@@ -74,19 +80,35 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         adcout >>= 1       # first bit is 'null' so drop it
         return adcout
 #main ioop
+
 def main():
          init()
          time.sleep(1)
-         while True:
-                  COlevel=readadc(mq7_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
-                  
-                  if GPIO.input(mq7_dpin):
-                           print("CO not leak")
-                           time.sleep(0.5)
-                  else:
-                           db.child("coValue").set(str("%.2f"%((COlevel/1024.)*1.2))+" V")
-               	           db.child("coDensity").set(str("%.2f"%((COlevel/1024.)*10))+" %")
-               	           time.sleep(2)
+         while True:             
+                COlevel=readadc(mq7_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+
+                if GPIO.input(mq7_dpin):
+                        print("CO not leak")
+                else:
+                        db.child("coValue").set(str("%.2f"%((COlevel/1024.)*1.2))+" V")
+                        db.child("coDensity").set(str("%.2f"%((COlevel/1024.)*10))+" %")
+	
+                GasLevel=readadc(mq2_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+	
+                if GPIO.input(mq2_dpin):
+                        print("Gas not leak")
+                else:
+                        db.child("gasValue").set(str("%.2f"%((GasLevel/1024.)))+" V")
+                        db.child("gasDensity").set(str("%.2f"%((GasLevel/1024.)*125))+" %")
+	
+                smokelevel=readadc(smokesensor_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
+
+                if GPIO.input(smokesensor_dpin):
+                        print("Gas not leak")
+                else:
+                        db.child("smokeValue").set(str("%.2f"%((smokelevel/1024.)/2.2))+" V")
+                        db.child("smokeDensity").set(str("%.2f"%((smokelevel/2048.)*100))+" %")
+                time.sleep(1)
 
 if __name__ =='__main__':
          try:
@@ -96,5 +118,7 @@ if __name__ =='__main__':
                   pass
 
 GPIO.cleanup()
+
+
          
          
